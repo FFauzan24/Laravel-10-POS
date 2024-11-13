302a53c39c65e1104bbd80030f1ef18f40c0d8f0
ffauzan <farhanfauzan1224@gmail.com>
2024-11-13 21:08:11 +0700

    setting layout master

--- routes/web.php
+++ routes/web.php
@@ -13,16 +13,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return redirect('/login');
-});
+// Route::get('/', function () {
+//     return redirect('/login');
+// });
 
+Route::get('/', fn() => redirect('/login'));
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
     Route::get('/dashboard', function () {
-        return view('dashboard');
+        return view('home');
     })->name('dashboard');
 });
